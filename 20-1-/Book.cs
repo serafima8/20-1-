3b0cd02025b77fcedebe4758e5df71138ddd8694
serafima8 <147659413+serafima8@users.noreply.github.com>
2024-04-22 09:47:46 +0300

    Add files via upload

---
+++ 20-1-/Book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_1_
+{
+    class Book <T>
+    {
+        /// <summary>
+        /// Поле названия книги
+        /// </summary>
+        private string  name;
+        /// <summary>
+        /// Поле цены книги
+        /// </summary>
+        private T price;
+        /// <summary>
+        /// Свойства Название
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Свойства Цена
+        /// </summary>
+        public T Price { get; set; }
+        /// <summary>
+        /// Метод который задает название книги
+        /// </summary>
+        /// <param name="name"></param>
+        public void Swap(string name)
+        {
+            Name = name;
+        }
+        /// <summary>
+        /// Метод с универсальным параметром, который задает цену книги
+        /// </summary>
+        /// <param name="price"></param>
+        public void SwapPrice(T price)
+        {
+            Price = price;
+        }
+        /// <summary>
+        /// Метод для вывода информации о книге
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"Название: {Name}\nЦена: {Price}";
+        }
+    }
+}
---
+++ 20-1-/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book<int> book = new Book<int>();
+            book.Swap("Отцы и дети");
+            book.SwapPrice((int)250);
+            Console.WriteLine(book.ToString());
+            Book<string> book1 = new Book<string>();
+            book1.Swap("Преступление и наказание");
+            book1.SwapPrice((string)"210");
+            Console.WriteLine(book1.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
