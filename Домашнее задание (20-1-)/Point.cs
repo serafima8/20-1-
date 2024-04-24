c50e3bb7c3bef0fa652620836bde3501b06f6547
serafima8 <147659413+serafima8@users.noreply.github.com>
2024-04-24 22:21:29 +0300

    Add files via upload

---
+++ Домашнее задание (20-1-)/Point.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Домашнее_задание__20_1__
+{
+    //Обобщенный класс Point.
+    class Point <T>
+    {
+        /// <summary>
+        /// Создание поля x
+        /// </summary>
+        private T x;
+        /// <summary>
+        /// Создание поля У
+        /// </summary>
+        private T y;
+        /// <summary>
+        /// Свойство Х
+        /// </summary>
+        public T X { get; set; }
+        /// <summary>
+        /// Свойство У
+        /// </summary>
+        public T Y { get; set; }
+        /// <summary>
+        /// Конструктор с параметрами 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public Point(T x, T y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        /// <summary>
+        ///  метод, выводящий значения внутренних полей класса.
+        /// </summary>
+        public void Show()
+        {
+            Console.WriteLine($"Координатная точка x: {X}\nКоординатная точка у: {Y}");
+        }
+    }
+}
---
+++ Домашнее задание (20-1-)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Домашнее_задание__20_1__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Первый объект с целыми параметрами 
+            Point<int> point = new Point<int>(3,6);
+            point.Show();
+            //Второй объект с вещественными параметрами 
+            Point<double> point1 = new Point<double>(5.5, 6.7);
+            point1.Show();
+
+            Console.ReadKey();
+        }
+    }
+}
