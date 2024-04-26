e567b4fee1945827f774de86c9a08361a70dcba2
serafima8 <147659413+serafima8@users.noreply.github.com>
2024-04-26 13:56:52 +0300

    Add files via upload

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_2_
+{
+    class Calculator 
+    {
+        public dynamic A { get; set; }
+        public dynamic B { get; set; }
+        public dynamic Add(dynamic a, dynamic b)
+        {
+            return a + b;
+        }
+        public dynamic Substract(dynamic a, dynamic b)
+        {
+            return a - b;
+        }
+        public dynamic Multiply(dynamic a, dynamic b)
+        {
+            return a * b;
+        }
+        public dynamic Division(dynamic a, dynamic b)
+        {
+            if (b == 0)
+            {
+                return "Ошибка. На ноль делить нельзя";
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine($"Сложение: {calculator.Add(5,4)}");
+            Console.WriteLine($"Вычитание: {calculator.Substract(6,5.5)}");
+            Console.WriteLine($"Умножение: {calculator.Multiply(2,9)}");
+            Console.WriteLine($"Деление: {calculator.Division(18,0)}");
+            Console.ReadKey();
+        }
+    }
+}
